b25cdbda6148ecff378ecebfe14edbeda25ad223
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-05-26 00:16:00 +0600

    new custorm first component create

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import ExpenceItem from './Components/ExpenceItem.js'
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Let us start</h1>
+      <p> Life is very awsome!!</p>
+
+      <ExpenceItem></ExpenceItem>
     </div>
   );
 }
 
 export default App;
+
---
+++ react-complete-guide/src/Components/ExpenceItem.js
@@ -0,0 +1,7 @@
+function ExpenceItem(){
+    return (
+        <h1>This is custom component</h1>
+    );
+}
+
+export default ExpenceItem;
