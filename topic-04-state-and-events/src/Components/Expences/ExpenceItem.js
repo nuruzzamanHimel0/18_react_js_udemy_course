663ff9febb8ca564ee1c1866817c81a72c00d09c
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-06-05 01:08:39 +0600

    useState react hook uses

--- topic-04-state-and-events/src/Components/Expences/ExpenceItem.js
+++ topic-04-state-and-events/src/Components/Expences/ExpenceItem.js
@@ -1,17 +1,24 @@
+import React, { useState } from 'react'
 import ExpenceDate from './ExpenceDate.js'
 import Card from '../UI/Card.js'
 import './ExpenceItem.css'
 
 const ExpenceItem = (props) =>{
-
+    const [title, setTitle] = useState ( props.title);  
+ 
+    const clickHandler = () => {
+        setTitle ( 'Update title in here' );
+    }
     return (
        <Card className='experience-item'>
            <ExpenceDate datetime={props.datetime} />
            
            <div className='experience-item-price'>
-               <h2> {props.title} </h2>
+               <h2> {title} </h2>
                <div>${props.amount}</div>
+               
            </div>
+           <button  onClick={clickHandler} >Change Title</button>
        </Card>
     );
 }
