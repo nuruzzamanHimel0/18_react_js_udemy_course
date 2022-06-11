b34bfe7cd70c2c8fa628aad6de4a2833d2bc0d0e
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-06-11 12:28:43 +0600

    005 Outputting Conditional Content

--- topic-05-Rendering-Lists-and-Conditional-Content/src/App.js
+++ topic-05-Rendering-Lists-and-Conditional-Content/src/App.js
@@ -9,7 +9,10 @@ const DUMMEY_EXPENCES = [
   { id:1, title:"Car Insurence", datetime :new Date(2021,5, 21), amount: 275.01 },
   { id:2, title:"Book Insurence", datetime :new Date(2021,5, 21), amount: 2735.01 },
   { id:3, title:"pancil Insurence", datetime :new Date(2021,5, 21), amount: 5275.01 },
-  { id:4, title:"Plan Insurence", datetime :new Date(2021,5, 21), amount: 5275.01 }
+  { id:4, title:"Plan Insurence", datetime :new Date(2021,5, 21), amount: 5275.01 },
+  { id:5, title:"Cardd Insurence", datetime :new Date(2022,5, 21), amount: 275.01 },
+  { id:5, title:"Care Insurence", datetime :new Date(2018,5, 21), amount: 275.01 },
+  { id:5, title:"Car34343 Insurence", datetime :new Date(2019,5, 21), amount: 275.01 },
 ];
 
 const App = ()=>{
--- topic-05-Rendering-Lists-and-Conditional-Content/src/Components/Expences/Expence.js
+++ topic-05-Rendering-Lists-and-Conditional-Content/src/Components/Expences/Expence.js
@@ -12,12 +12,31 @@ const Expence = (props) => {
     const filterExpences = props.expences.filter( (expence) => {
         return expence.datetime.getFullYear().toString() === filterYear;
     } );
+    // 005 Outputting Conditional Content
+    let expencesContent = <p> No expences found </p>;
+    if(filterExpences.length > 0){
+        expencesContent =  filterExpences.map(
+            (expence) => 
+            <ExpenceItem 
+            key={expence.id}
+
+            amount={expence.amount}
+            title={expence.title}
+            datetime ={expence.datetime} />
+        );
+    }
     console.log(filterExpences, filterYear);
     return (
         <Card className="hole_expences">
             <ExpenceFilter yearselected={filterYear} onChangeFilter = {filterYearChange} />
+            {/* // third process ...  */}
+            {expencesContent}
+
+
+            {/* // secound process... */}
+            {/* {filterExpences.length === 0 && <p> No expences found </p> }
             {
-                filterExpences.length === 0 ? ' No expences found ':
+                filterExpences.length > 0 &&
 
                 filterExpences.map(
                     (expence) => 
@@ -29,7 +48,22 @@ const Expence = (props) => {
                     datetime ={expence.datetime} />
                 )
                 
-            }
+            } */}
+            {/* // first process.... */}
+            {/* {
+                filterExpences.length > 0 ? ' No expences found ':
+
+                filterExpences.map(
+                    (expence) => 
+                    <ExpenceItem 
+                    key={expence.id}
+
+                    amount={expence.amount}
+                    title={expence.title}
+                    datetime ={expence.datetime} />
+                )
+                
+            } */}
             {/* // {
             //     props.expences.map(
             //         (expence) => 
